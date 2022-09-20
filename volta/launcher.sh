8b11131da6c7ee3fd14afd9009c61c81993cd747
Artem Kryvchun <door2cosmos@gmail.com>
2022-09-20 19:34:29 +0300

    feat(volta): add completion function

--- volta/launcher.sh
+++ volta/launcher.sh
@@ -2,3 +2,8 @@
 
 export VOLTA_HOME="$HOME/.volta"
 export PATH="$VOLTA_HOME/bin:$PATH"
+
+volta_completion() {
+	volta completions bash >"$DOTFILES_TMP/volta_completion.sh"
+	. "$DOTFILES_TMP/volta_completion.sh"
+}

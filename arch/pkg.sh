2e7c4b62ae7d4586b339613f25321af7b702d263
Artem Kryvchun <door2cosmos@gmail.com>
2022-09-20 19:27:04 +0300

    fix(arch): fix pkg

--- arch/pkg.sh
+++ arch/pkg.sh
@@ -2,40 +2,43 @@
 
 pacman_install() {
 	log "Pacman is installing packages"
-	xargs pacman -S --needed --noconfirm <./arch/explicit_packages.txt
+	xargs sudo pacman --sync --needed --noconfirm <./arch/native_packages.txt
 }
 
 pacman_update() {
 	log "Pacman is updating packages"
-	sudo pacman -Syu
+	sudo pacman --sync --refresh --sysupgrade
 }
 
 pacman_refresh() {
 	goto "${BASH_SOURCE[0]}"
 
-	log "Refreshing arch/explicit_packages.txt"
-	pacman -Qqe >explicit_packages.txt
+	log "Refreshing arch/native_packages.txt"
+	pacman --query --quiet --explicit --native >native_packages.txt
 
 	log "Refreshing arch/foreign_packages.txt"
-	pacman -Qqem >foreign_packages.txt
+	pacman --query --quiet --explicit --foreign >foreign_packages.txt
 
-	sudo pkgfile -u # TODO: Why do I need it?
 	back
 }
 
 yay_install() {
-	local DOWNLOADS_DIR="$HOME"/Downloads
-
 	log "Installing yay"
-	pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd "$DOWNLOADS_DIR" && makepkg -si && cd .. && rm -rf yay
+	cd || exit
+	git clone https://aur.archlinux.org/yay.git
+	cd yay || exit
+	makepkg --syncdeps --needed --rmdeps --install --noconfirm
+	cd || exit
+	rm -rf yay
 
 	log "Yay is installing packages"
-	xargs yay -S <./arch/foreign_packages.txt
+	gotodot
+	xargs yay --sync --needed --noconfirm --nodiffmenu --removemake --cleanafter <./arch/foreign_packages.txt
 }
 
 yay_update() {
 	log "Yay is updating packages"
-	yay -Sua --answerdiff None --answerclean All
+	yay -Sua --noconfirm --nodiffmenu --removemake --cleanafter
 }
 
 yay_clean() {

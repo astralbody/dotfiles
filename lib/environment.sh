9208e4c4bb22fb23f1ec9ff95b1c07fc02fee48a
Artem Kryvchun <door2cosmos@gmail.com>
2022-09-23 12:18:51 +0300

    feat(lib): move exports from install script

--- lib/environment.sh
+++ lib/environment.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
 export PS1='[\u@\h \W]\$ '
+
+export PROJECTS=$HOME/Projects
+export DOWNLOADS="$HOME"/Downloads
+
+export DOTFILES=$PROJECTS/dotfiles
+export DOTFILES_TMP=$HOME/.dotfiles.tmp
+
+export BIN=$HOME/.local/bin
+export PATH=$PATH:"$HOME"/.local/bin

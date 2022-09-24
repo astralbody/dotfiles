581f28a358a6ef09905346ec08363edd5969fcef
Artem Kryvchun <door2cosmos@gmail.com>
2022-09-24 16:30:07 +0300

    feat(debian): add update function to pkg

--- debian/pkg.sh
+++ debian/pkg.sh
@@ -2,5 +2,13 @@
 
 apt_install() {
 	log "Apt is installing packages"
+	apt_update
 	xargs sudo apt -y install <./debian/packages.txt
+	sudo ln -sf "$(which fdfind)" "$BIN"/fd
+}
+
+apt_update() {
+	sudo apt update -y
+	sudo apt full-upgrade -y
+	sudo apt clean
 }
